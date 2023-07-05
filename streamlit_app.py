b2517aab6350f06b0435e1ea2a42270962fc2daa
Vaibhav Jain <jainvaibhav62@gmail.com>
2023-07-05 10:39:43 -0500

    Update streamlit_app.py
    updating code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.header(' 🥣 Breakfast Menu')
 streamlit.text('  🥗 Omega 3 & Blueberry Oatmeal')
@@ -17,5 +18,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
+streamlit.header('Fruityvice Fruit Advice!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
